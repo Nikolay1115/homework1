73fafa8c9fc868dd5ce39edcd7bd2b2a62545e71
Nikolai <kolia54650@gmail.com>
2024-12-11 23:00:23 +0300

    Прописал номера заданий и выполнил пятое

--- hw-n2.js
+++ hw-n2.js
@@ -1,16 +1,19 @@
+// Задание 1
 let a = 10;
 alert (a);
 alert (a = 20);
-
+// Задание 2
 let iphone =  2007;
 alert (iphone);
-
+// Задание 3
 let js = "Брендан Эйх";
 alert (js);
-
+// Задание 4
 let x = 10;
 let y = 2;
 alert (x + y);
 alert (x - y);
 alert (x * y);
 alert (x / y);
+// Задание 5
+alert (y ** 5);
