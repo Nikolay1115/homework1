66e0625fff9238843aa7f27a76e6c3d17b824eba
Nikolai <kolia54650@gmail.com>
2024-12-30 14:23:48 +0300

    сделал первое задание

---
+++ hw-n3.js
@@ -0,0 +1,6 @@
+let password = 'skypro';
+let userPassword = prompt('введите пароль');
+if (userPassword === password) {
+    alert ('Пароль введен верно');
+} 
+else {alert('Пароль введен неправильно');}
